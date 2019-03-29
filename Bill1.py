25d898ba62acdb84b08efb627b192e5e4930f518
adityaa25 <49044503+adityaa25@users.noreply.github.com>
2019-03-29 16:37:43 +0530

    Add files via upload

---
+++ Bill1.py
@@ -0,0 +1,20 @@
+a = 32.9875
+b = 21
+c = 210.00
+d = 12.939
+e = 21.22
+f = 90.32
+
+a1 = 'sugar'
+b1 = 'salt'
+c1 = 'rice'
+d1 = 'dal'
+e1 = 'peas'
+f1 = 'misc'
+
+print("\tbill".upper())
+
+print("{}\t :   Rs.{}".format(a1.capitalize(),str(a)[0:5]))
+print("{}\t :   Rs.{}".format(b1.capitalize(),str(b)[0:5]))
+print("{}\t :   Rs.{}".format(c1.capitalize(),str(c)[0:5]))
+print("{}\t :   Rs.{}".format(d1.capitalize(),str(d)[0:5]))
