162cddaa21c14ede3a64fbd138bdbb274d340346
adityaa25 <49044503+adityaa25@users.noreply.github.com>
2019-04-02 16:17:43 +0530

    Add files via upload

---
+++ BillMax.py
@@ -0,0 +1,45 @@
+print("\tLifestryle Internatina Pvt Ltd\n\t    (Max Retail Division)\n\t  F-47, 1st Floor, Survey#64\n\t    Hyderabad - 500081\n\t\tTelangana")
+print("BIll Location : TELANGANA")
+print("GSTIN -385983GH3748\n")
+print("_____________________________")
+shorts=349.00
+top=269.00
+plastic=5.00
+c=shorts+top+plastic
+print("\n\n")
+print("Name : Aditya\nMobile : 94994994944\nLandmark Rewards No : 769407334")
+print("\n\n\n")
+print("\t\tTax Invoice/Bill of Supply\n")
+print("ITEM/HSN/Desc  MRP\tDisc%\tRSP\tQty\tT%")
+print("100000675084\t62031910\tshorts\n\t\t"+str(shorts)+"\t0.0\t349.00\t1\tA,B")
+print("100000637547\t62031663\tKnit Top SS\n\t\t"+str(top)+"\t0.0\t269.00\t1\tA,B")
+print("100000736487\t32849849\tPlastic - Medi\n\t\t"+str(plastic)+"\t0.0\t5.00\t1\tC,D")
+print("\n\n")
+print("Total Amt\t\t\t"+str(c))
+print("Total Rounded\t\t\t"+str(c))
+print("Rounded Off Amt\t\t\t0.00")
+print(" ICICI-ONLINE\t\t\t"+str(c))
+print("  Account: 3530\n  Auth: 047733()")
+print("Total Tender\t\t\t"+str(c))
+print("Change Due\t\t\t0.00")
+print("Total Numner of Items/Qty:\t3/ 3")
+print("__________________________________________")
+
+A=14.71
+B=14.71
+C=0.38
+D=0.38
+T=A+B+C+D
+print("\t\tTax Details")
+print(" Tax Desc\t\tTaxable\t  Tax")
+print(f"A SGST_  (2.5%)\t\t588.57\t  {A}") #usage of f-string
+print(f"B CGST_  (2.5%)\t\t588.57\t  {B}")
+print(f"C SGST_    (9%)\t\t  4.24\t   {C}")
+print(f"D CGST_    (9%)\t\t  4.24\t   {D}")
+print("\t\t\t\t  _______")
+print(f"Total Tax Value\t\t\t   {T}")
+print("\n__________________________________________")
+print("Str:02073 Reg:102 Tran:0067105 Till:102")
+print("12-Sep-2018 17:22:36  Cashier : 1077103")
+print("Invoice No : 0274858028485938")
+
---
+++ Lists1.py
@@ -0,0 +1,14 @@
+sample = ['aditya','srinivas','roll','college',34]
+print(sample)
+
+sample.append(23)
+print(sample)
+
+
+print(sample.remove('srinivas'))
+print(sample)
+
+sample.insert(1,'srinivas')
+print(sample)
+
+print(sample[1:5])
